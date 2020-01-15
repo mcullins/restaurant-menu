9c5670c0c9874cbbd273a1832f244d9b1b713882
Megan Cullins <mlcullins@gmail.com>
2020-01-15 14:54:36 -0600

    fixed add item function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        MenuItem pizza = new MenuItem("Pizza", 10.00);
+        MenuItem pizza = new MenuItem("Pepperoni Pizza", 10.00);
+        MenuItem salad = new MenuItem("Harvest Salad", 8.00);
         Menu altMenu = new Menu();
-        altMenu.addMenuItem(altMenu, pizza);
+        altMenu.addMenuItem(pizza);
+        altMenu.addMenuItem(salad);
+        altMenu.toPrint();
+        pizza.setPrice(11.00);
+        altMenu.toPrint();
+
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,25 +1,27 @@
 
+import javax.sound.midi.MidiUnavailableException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 
 
 public class Menu {
-    public ArrayList<MenuItem> name;
+    public ArrayList<MenuItem> item;
     private Date lastUpdated;
     private static int menuId = 1;
 
     public Menu (){
-        this.name = new ArrayList<MenuItem>();
+        this.item = new ArrayList<MenuItem>();
         this.lastUpdated = new Date();
         this.menuId = menuId;
         menuId++;
     }
 
     public ArrayList<MenuItem> getName(){
-        return name;
+        return item;
     }
     public void setName(ArrayList<MenuItem> name){
-        this.name = name;
+        this.item = item;
         this.lastUpdated = new Date();
     }
     public Date getLastUpdated(){
@@ -29,10 +31,16 @@ public class Menu {
         return menuId;
     }
 
-    public void addMenuItem(ArrayList<MenuItem> menu, MenuItem menuItem){
-        menu.add(menuItem);
+    public void addMenuItem(MenuItem menuItem){
+        item.add(menuItem);
     }
-    public void deleteMenuItem(ArrayList<MenuItem> menu, MenuItem menuItem){
-        menu.remove(menuItem);
+    public void deleteMenuItem(MenuItem menuItem){
+        item.remove(menuItem);
+    }
+    public void toPrint(){
+        for(MenuItem itemName : item) {
+           System.out.println(itemName.getName() + " $" + itemName.getPrice() +
+                   "(Last Updated: " + itemName.getLastUpdated() + ").");
+        }
     }
 }
